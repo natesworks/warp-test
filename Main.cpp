334864c87008d7cbc9ffc729ce027849bf212fd9
Natesworks <contact@natesworks.com>
2025-03-28 16:08:57 +0100

    initial commit

---
+++ Main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <thread>
+
+#include "warpengine/Game.h"
+#include "warpengine/Types/Object.h"
+#include "warpengine/Components/Rectangle.h"
+#include "warpengine/Components/FilledRectangle.h"
+#include "warpengine/Components/Ellipse.h"
+#include "warpengine/Components/FilledEllipse.h"
+#include "warpengine/Components/Button.h"
+
+void onClick(Game& game, Object& object)
+{
+    int randomX = rand() % game.getWindowWidth();
+    int randomY = rand() % game.getWindowHeight();
+    object.setPosition(Vector2(randomX, randomY));
+}
+
+int main()
+{
+    Game game = Game(960, 540, "Portal 3");
+    game.addObject(Object(&game, Vector2(0, 0), Vector2(100, 100), 25, RGB(0, 0, 255)));
+    Object* object = game.getObject(0);
+    FilledRectangle rectangle(object);
+    object->addComponent(std::move(rectangle));
+    Button button(object, [&game, &object]() { onClick(game, *object); });
+    object->addComponent(std::move(button));
+    Rectangle hitbox(object);
+    object->addComponent(std::move(hitbox));
+
+    game.addObject(Object(&game, Vector2(450, 450), Vector2(100, 100), 25, RGB(255, 0, 0)));
+    Object* object2 = game.getObject(1);
+    FilledEllipse ellipse(object2);
+    object2->addComponent(std::move(ellipse));
+    Button button2(object2, [&game, &object2]() { onClick(game, *object2); });
+    object2->addComponent(std::move(button2));
+    //Rectangle hitbox2(object);
+    //object2->addComponent(std::move(hitbox2));
+
+    game.start();
+
+    while (true) {}
+
+    return 0;
+}
