1ceb0b971127a1e1086b24c4dcfcc3ab805d1641
Natesworks <contact@natesworks.com>
2025-03-29 14:07:33 +0100

    Update for changes

--- Main.cpp
+++ Main.cpp
@@ -10,35 +10,34 @@
 #include "warpengine/Components/PlayerMovement.h"
 #include "warpengine/Components/BoxCollider.h"
 
-void onClick(Game& game, Object& object)
+void onClick(Game &game, Object *object)
 {
     int randomX = rand() % game.getWindowWidth();
     int randomY = rand() % game.getWindowHeight();
-    object.setPosition(Vector2(randomX, randomY));
+    object->setPosition(Vector2(randomX, randomY));
 }
 
 int main()
 {
     Game game = Game(960, 540, "Portal 3");
-    game.addObject(Object(&game, Vector2(0, 0), Vector2(100, 100), 0, RGB(0, 0, 255)));
-    Object* player = game.getObject(0);
-    FilledRectangle rectangle(player);
-    player->addComponent(std::move(rectangle));
-    PlayerMovement playerMovement(player, 15);
-    player->addComponent(std::move(playerMovement));
-    BoxCollider collider(player, false);
-    player->addComponent(std::move(collider));
-    //Rectangle hitbox(object);
-    //object->addComponent(std::move(hitbox));
+    Object *player = game.addObject(new Object(&game, Vector2(100, 100), Vector2(100, 100), 0, RGB(0, 0, 255)));
+    player->addComponent(new FilledRectangle(player));
+    player->addComponent(new PlayerMovement(player, 15));
+    player->addComponent(new BoxCollider(player));
 
-    game.addObject(Object(&game, Vector2(450, 450), Vector2(100, 100), 0, RGB(255, 0, 0)));
-    Object* object2 = game.getObject(1);
-    FilledEllipse ellipse(object2);
-    object2->addComponent(std::move(ellipse));
-    Button button2(object2, [&game, &object2]() { onClick(game, *object2); });
-    object2->addComponent(std::move(button2));
-    Rectangle hitbox2(object2);
-    object2->addComponent(std::move(hitbox2));
+    Object *circle = game.addObject(new Object(&game, Vector2(450, 450), Vector2(100, 100), 0, RGB(255, 0, 0)));
+    circle->addComponent(new FilledEllipse(circle));
+    circle->addComponent(new Button(circle, [&game, circle]()
+                                    { onClick(game, circle); }));
+
+    Object *square = game.addObject(new Object(&game, Vector2(200, 200), Vector2(100, 100), 0, RGB(0, 255, 0)));
+    square->addComponent(new Rectangle(square));
+    square->addComponent(new BoxCollider(square, [&game, &square]()
+                          { std::cout << "Collided with square" << std::endl; }));
+
+    Object *square2 = game.addObject(new Object(&game, Vector2(400, 700), Vector2(100, 100), 0, RGB(255, 255, 0)));
+    square2->addComponent(new Rectangle(square2));
+    square2->addComponent(new BoxCollider(square2));
 
     game.start();
 
